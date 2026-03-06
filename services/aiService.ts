a3991057aeb280a9c22ac39c3eb62789a9300f13
S-A-T-Y-A <satyanandan846@gmail.com>
2026-03-06 16:06:20 -0500

    base64

--- services/aiService.ts
+++ services/aiService.ts
@@ -155,7 +155,7 @@ export const generateVideoPost = async (prompt: string, onProgress: (status: Vid
             if (images.length === 1) {
                 const { mimeType, data } = dataUrlToAiPart(images[0]);
                 generationOptions.image = {
-                    imageBytes: data,
+                    bytesBase64Encoded: data,
                     mimeType: mimeType
                 };
             } else {
@@ -166,7 +166,7 @@ export const generateVideoPost = async (prompt: string, onProgress: (status: Vid
                     const { mimeType, data } = dataUrlToAiPart(img);
                     return {
                         image: {
-                            imageBytes: data,
+                            bytesBase64Encoded: data,
                             mimeType: mimeType
                         },
                         referenceType: 'ASSET' // Using 'ASSET' as a generic reference type
